15897fd882d95629f985c0aa39c1c5bbea67592c
mahesh247 <deathdatruth247@gmail.com>
2018-04-04 08:10:53 +0545

    jQuery function added

--- assets/converter-init.js
+++ assets/converter-init.js
@@ -3,7 +3,7 @@
 	Date converter jQuery plugin by Mahesh Maharjan 2015
 	http://www.mahesh247.com.np
 */
-
+jQuery(function($){
 var converter = $('body').myConverter();
 	$('#toBS').on('click', function(){
 		var YY = $('select#eYY').val();
@@ -19,6 +19,7 @@ var converter = $('body').myConverter();
 		var MM = $('select#nMM').val();
 		MM = parseInt(MM) + parseInt(1);
 		var DD = $('select#nDD').val();
-		var resultnDate = converter.convertToAD(YY, MM, DD);
-		$('span#result').text(' | Result => '+resultnDate);
+		var resulteDate = converter.convertToAD(YY, MM, DD);
+		$('span#result').text(' | Result => '+resulteDate);
 	});
+});
